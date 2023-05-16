be36a4bafab9918dd1eebaa7ede03639b5afb791
kato223272 <223272@ids.upchiapas.edu.mx>
2023-05-16 00:08:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+    <h4 className="App- datos">
+
+      Datos del cliente:
+      <br></br>
+      hola
+    </h4>
+       
+
       </header>
+
     </div>
   );
 }
